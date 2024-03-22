30b36774bf452d5ff52df68965322e6372b8520f
Thiago Firsen <thiagofirsen47@hotmail.com>
2024-03-21 21:04:22 -0300

    refactor: move mock

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,7 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "@/router";
 import { useState } from "react";
+import { mockCategoriesPictures } from "@/mocks";
 
 export const Home = () => {
   const { products } = useProducts();
@@ -16,18 +17,6 @@ export const Home = () => {
   const [filteredProducts, setFilteredProducts] = useState<
     Product[] | undefined
   >([]);
-
-  const mockCategoriesPictures = {
-    electronics:
-      "https://images.unsplash.com/photo-1517077304055-6e89abbf09b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=80",
-    jewelery:
-      "https://images.unsplash.com/photo-1600721391689-2564bb8055de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=80",
-    "men's clothing":
-      "https://images.unsplash.com/photo-1520975916090-3105956dac38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=80",
-    "women's clothing":
-      "https://plus.unsplash.com/premium_photo-1661777373172-65269ff71d2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=80",
-    all: "https://images.unsplash.com/photo-1633409361618-c73427e4e206?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  };
 
   const renderItemCategory: ListRenderItem<Category> = ({ item, index }) => {
     return (
